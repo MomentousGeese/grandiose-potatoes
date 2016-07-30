0e81e1b26d2c9b7fa63ba86604fc74a9a9b7675b
Greg Roche <grroche.us@gmail.com>
2016-07-30 10:48:34 -0700

    Begin implementing socket.io

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
+//Requires
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes');
 const session = require('express-session');
+const http = require('http');
+const socket = require('socket.io');
+
+//Init
 const app = express();
+const server = http.createServer(app);
+const io = socket.listen(server);
+
 
 app.use(express.static(`${__dirname}/../client`));
 app.use(session({ secret: 'test code' }));
@@ -11,11 +19,16 @@ app.use(session({
   secret: 'test code',
 }));
 
-app.port = process.env.PORT || 3000;
+// app.port = process.env.PORT || 3000;
 
-app.listen(app.port, () => {
-  console.log('We are listening!');
-});
+// app.listen(app.port, () => {
+//   console.log('We are listening!');
+// });
+
+const port = 3000;
+
+server.listen(process.env.PORT || port);
+console.log('Listening on port 3000');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,3 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
 
 app.use(bodyParser.json());
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+})
